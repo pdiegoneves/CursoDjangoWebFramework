eb0f9f081f4e77796bb4c371153690bddf222de3
Diego Neves <pdcassiano@gmail.com>
2023-06-26 06:09:30 -0300

    templates

--- projeto1/recipes/views.py
+++ projeto1/recipes/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', status=201, context={
+        'name': 'Luiz Otávio', 
+    })
 
 
 def contato(request):

a87cb4a2facbcb232243ec8aedfe212d5aabbaa2
Diego Neves <pdcassiano@gmail.com>
2023-07-12 16:57:47 -0300

    partial - head

--- projeto1/recipes/views.py
+++ projeto1/recipes/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html', status=201, context={
+    return render(request, 'recipes/pages/home.html', status=201, context={
         'name': 'Luiz Otávio', 
     })

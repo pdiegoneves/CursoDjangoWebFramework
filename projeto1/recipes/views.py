86e47068fa3a7494c49248dded05612b6472b30d
Diego Neves <pdcassiano@gmail.com>
2023-08-04 07:10:13 -0300

    recipe-view

--- projeto1/recipes/views.py
+++ projeto1/recipes/views.py
@@ -9,6 +9,6 @@ def home(request):
     })
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/home.html', status=201, context={
+    return render(request, 'recipes/pages/recipe-view.html', status=201, context={
         'name': 'Luiz Otávio', 
     })

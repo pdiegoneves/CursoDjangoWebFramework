e13c553332214c455391f0edd46bba503513d5bf
Diego Neves <pdcassiano@gmail.com>
2023-08-16 15:50:18 -0300

    usando o ORM

--- projeto1/recipes/views.py
+++ projeto1/recipes/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 # Create your views here.
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
+
+
     return render(request, 'recipes/pages/home.html', status=201, context={
-        'recipes': [make_recipe() for _ in range(10)], 
+        'recipes': recipes, 
     })
 
 def recipe(request, id):

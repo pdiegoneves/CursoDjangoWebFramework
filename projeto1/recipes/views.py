0791bbfe688b3cf26b639d1d51d5652725564132
Diego Neves <pdcassiano@gmail.com>
2023-08-17 14:33:18 -0300

    not found category

--- projeto1/recipes/views.py
+++ projeto1/recipes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 
@@ -12,8 +12,13 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id = category_id, is_published=True).order_by('-id')
+
+    if not recipes:
+        raise Http404('Not Found 🥲')
+
     return render(request, 'recipes/pages/category.html', status=201, context={
         'recipes': recipes, 
+        'title': f'{recipes.first().category.name} - Category |'
     })
 
 def recipe(request, id):

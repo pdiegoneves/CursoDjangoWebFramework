3cdcf76e1b0678aeb93fdbeb5b594bd012aa2a62
Diego Neves <pdcassiano@gmail.com>
2023-08-25 09:49:39 -0300

    correção do recipe e get_list_or_404

--- projeto1/recipes/views.py
+++ projeto1/recipes/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-from django.http import Http404
-from utils.recipes.factory import make_recipe
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Recipe
 
 # Create your views here.
@@ -11,18 +9,17 @@ def home(request):
     })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id = category_id, is_published=True).order_by('-id')
-
-    if not recipes:
-        raise Http404('Not Found 🥲')
+    recipes = get_list_or_404(Recipe.objects.filter(category__id = category_id, is_published=True).order_by('-id')
+)
 
     return render(request, 'recipes/pages/category.html', status=201, context={
         'recipes': recipes, 
-        'title': f'{recipes.first().category.name} - Category |'
+        'title': f'{recipes[0].category.name} - Category |'
     })
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(id = id, is_published=True).first()
     return render(request, 'recipes/pages/recipe-view.html', status=201, context={
-        'recipe': make_recipe(), 
+        'recipe': recipe, 
         'is_detail_page': True,
     })
